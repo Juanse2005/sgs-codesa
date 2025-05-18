5aaaff1c8cb13e24c54e0d374c547127d5e7deee
Juan Sebastian <juannino907@gmail.com>
2025-05-18 06:40:33 -0500

    [JN] - Person entity and validation aggregate

---
+++ backend/src/main/java/com/codesa/backend/entity/Persona.java
@@ -0,0 +1,41 @@
+package com.codesa.backend.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_persona;
+
+    @NotBlank
+    private String nombre;
+
+    @NotBlank
+    private String apellido;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDate fecha_nacimiento;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{10}")
+    private String telefono;
+
+}
