54c1b926b4a8402845ec5447be659fc13732d39f
Juan Sebastian <juannino907@gmail.com>
2025-05-19 19:13:00 -0500

    [JN] - Added lazy loading in routes and new login component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+
+  },
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./views/login/login.module').then(m => m.LoginModule)
+          }
+        ]
+      }
+    ]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
